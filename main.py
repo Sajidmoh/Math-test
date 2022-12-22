e89c1126be729e4362b70165447bb3578efac49d
saj moh <109856353+Sajidmoh@users.noreply.github.com>
2022-12-22 20:03:57 +0000

    Added random number generator to generate questions and a user friendly begining

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 import random
 
-number1 = random.randint(1,100)
+
+def getMathQuestion():
+    number1 = random.randint(1, 100)
+    number2 = random.randint(1, 100)
+    question = str(number1) + " + " + str(number2)
+    answer = number1 + number2
+    print(question)
+    givenAnswer = int(input("enter your answer: "))
+    if answer == givenAnswer:
+        print("your answer is correct")
+    else:
+        print("incorrect")
+
+
+def getBegining():
+    questionCount = random.randint(5, 10)
+    print("there are ", questionCount, " question in your test")
+    print("         y or n ")
+    check = input("are you ready to start: ")
+    if check == "y":
+        for i in range(questionCount):
+            getMathQuestion()
+    elif check == "n":
+        print("come back when youre ready")
+        exit()
+    else:
+        print("you typed an ivalid input please try again")
+        getBegining()
+
+
+getBegining()
