4391faa95fcb818a89d7514db1ee2e302d5568e6
saj moh <109856353+Sajidmoh@users.noreply.github.com>
2022-12-27 19:25:45 +0000

    added score and time taken

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import random
+from time import time
+score = 0
+questionCount = random.randint(1, 3)
+operators = []
 
 
-def getMathQuestion():
-    number1 = random.randint(1, 100)
-    number2 = random.randint(1, 100)
-    question = str(number1) + " + " + str(number2)
-    answer = number1 + number2
-    print(question)
-    givenAnswer = int(input("enter your answer: "))
-    if answer == givenAnswer:
-        print("your answer is correct")
-    else:
-        print("incorrect")
-
-
-def getBegining():
-    questionCount = random.randint(5, 10)
-    print("there are ", questionCount, " question in your test")
-    print("         y or n ")
-    check = input("are you ready to start: ")
-    if check == "y":
-        for i in range(questionCount):
-            getMathQuestion()
-    elif check == "n":
-        print("come back when youre ready")
-        exit()
-    else:
-        print("you typed an ivalid input please try again")
-        getBegining()
-
-
-getBegining()
+print("there are ", questionCount, " question in your test")
+print("         y or n ")
+check = input("are you ready to start: ")
+if check == "y":
+    start = time()
+    for i in range(questionCount):
+        number1 = random.randint(1, 100)
+        number2 = random.randint(1, 100)
+        question = str(number1) + " + " + str(number2)
+        answer = number1 + number2
+        print(question)
+        givenAnswer = int(input("enter your answer: "))
+        if answer == givenAnswer:
+            print("your answer is correct")
+            score += 1
+        else:
+            print("incorrect")
+    end = time()        
+elif check == "n":
+    print("come back when youre ready")
+    exit()
+else:
+    print("you typed an ivalid input please try again")
+   
+timeTaken = end - start
+print(f"timetaken: {timeTaken} " )
+print("your score was",score,"out of ",questionCount)
